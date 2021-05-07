137ac9f515a5760fa6bf346906b7a131e927f7df
MollyTong <62685671+MollyTong@users.noreply.github.com>
2021-05-07 16:15:22 +1200

    update class name
    update class name

--- lib/datasets/pascal_voc.py
+++ lib/datasets/pascal_voc.py
@@ -46,11 +46,10 @@ class pascal_voc(imdb):
             else devkit_path
         self._data_path = os.path.join(self._devkit_path, 'VOC' + self._year)
         self._classes = ('__background__',  # always index 0
-                         'aeroplane', 'bicycle', 'bird', 'boat',
-                         'bottle', 'bus', 'car', 'cat', 'chair',
-                         'cow', 'diningtable', 'dog', 'horse',
-                         'motorbike', 'person', 'pottedplant',
-                         'sheep', 'sofa', 'train', 'tvmonitor')
+                         'pedstrain', 'person on vehicle', 'car', 'bicycle',
+                         'motorbike', 'non motorized vehicle', 'static Person',
+                         'distractor', 'occluder', 'occluder on the ground', 'occluder full',
+                         'reflection')
         self._class_to_ind = dict(zip(self.classes, xrange(self.num_classes)))
         self._image_ext = '.jpg'
         self._image_index = self._load_image_set_index()
